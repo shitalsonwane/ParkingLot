bddacfb178dcb2c2b01385e9e745ae6ae2db314f
shitalSonwane <shitals177@gmail.com>
2020-04-27 18:13:51 +0530

    Refactored:code with beforeeach

--- test/ParkingLotSystemTest.js
+++ test/ParkingLotSystemTest.js
@@ -1,9 +1,12 @@
 let ParkingLotSystem=require('../src/ParkingLotSystem')
 let assert=require('chai').assert
+let parkingLotSystem
 describe('Testing for park vehicle',function(){
+    beforeEach(() => {
+        parkingLotSystem = new ParkingLotSystem()
+    })
     it('Test case for park the vehicle',function(){
         let car=new Object()
-        let parkingLotSystem=new ParkingLotSystem()
         assert.isTrue(parkingLotSystem.park(car))
     })
 })
