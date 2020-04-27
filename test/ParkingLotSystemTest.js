99217ae3e1e17b92de7d82215f6a2716b6f9ae41
shitalSonwane <shitals177@gmail.com>
2020-04-27 18:22:18 +0530

    Added:test case for undfied Unpark function

--- test/ParkingLotSystemTest.js
+++ test/ParkingLotSystemTest.js
@@ -1,12 +1,18 @@
 let ParkingLotSystem=require('../src/ParkingLotSystem')
 let assert=require('chai').assert
 let parkingLotSystem
-describe('Testing for park vehicle',function(){
+describe('Test cases for ParkingLotSystem',function(){
     beforeEach(() => {
         parkingLotSystem = new ParkingLotSystem()
     })
-    it('Test case for park the vehicle',function(){
+    //TEST CASE FOR PARK THE VEHICLE
+    it('should return true when vehicle is park',function(){
         let car=new Object()
         assert.isTrue(parkingLotSystem.park(car))
+    })
+    it('should return true when vehicle is unpark',function(){
+        let car=new Object()
+        parkingLotSystem.park(car)
+        assert.isFalse(parkingLotSystem.unpark(car))
     })
 })
