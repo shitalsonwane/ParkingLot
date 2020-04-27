43707e1d704ff127473caf173b4bff07af8dc839
shitalSonwane <shitals177@gmail.com>
2020-04-27 18:10:27 +0530

    Added:test case for park the vehicle

--- test/ParkingLotSystemTest.js
+++ test/ParkingLotSystemTest.js
@@ -4,6 +4,6 @@ describe('Testing for park vehicle',function(){
     it('Test case for park the vehicle',function(){
         let car=new Object()
         let parkingLotSystem=new ParkingLotSystem()
-        assert.equal(parkingLotSystem.park(car),false)
+        assert.isTrue(parkingLotSystem.park(car))
     })
 })
