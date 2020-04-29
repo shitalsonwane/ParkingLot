01393201643526c9409f839f662cbc9a7dd0345d
shitalSonwane <shitals177@gmail.com>
2020-04-29 14:41:52 +0530

    AddedLempty isparkinglotavailable function

--- src/ParkingLotOwner.js
+++ src/ParkingLotOwner.js
@@ -2,5 +2,8 @@ class ParkingLotOwner {
     isParkingLotFull() {
         console.log('notification to vehicle owner :- parking lot is full.')
     }
+    isParkingLotAvailable(){
+
+    }
 }
 module.exports = ParkingLotOwner 
